ad956b1772de6e59f295410fbab71efee00692fa
IDONTSUDO <idontsudo@gmail.com>
2019-10-02 14:20:47 +0300

    prod build

---
+++ front/CRM/build/precache-manifest.2b2c9a337d080e6144c725ed5d36c75b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd4482e96c7acbe45313bdb9569d2c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eb556729022c6bdd3f9",
+    "url": "/static/css/2.f97d4d2e.chunk.css"
+  },
+  {
+    "revision": "192a599d5b499bcf31c3",
+    "url": "/static/css/main.0062db62.chunk.css"
+  },
+  {
+    "revision": "1eb556729022c6bdd3f9",
+    "url": "/static/js/2.e7681ff6.chunk.js"
+  },
+  {
+    "revision": "192a599d5b499bcf31c3",
+    "url": "/static/js/main.0f92ff0a.chunk.js"
+  },
+  {
+    "revision": "93d508e4f18be890c042",
+    "url": "/static/js/runtime~main.10b6d9aa.js"
+  },
+  {
+    "revision": "f25142eb8764e2146fb6ec36e012cdb7",
+    "url": "/static/media/default.f25142eb.png"
+  }
+]);
